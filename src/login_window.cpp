ac015642dd0202ce2e9176f7ac06948a8019f596
joseph-kt-mbl <youcefabdellaouidev@gmail.com>
2024-02-14 01:59:35 +0100

    fix5

--- src/login_window.cpp
+++ src/login_window.cpp
@@ -1,46 +1,33 @@
+#include <gtk/gtk.h>
 #include "login_window.h"
 
 LoginWindow::LoginWindow() {
-    // Create the window
     window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
     gtk_window_set_title(GTK_WINDOW(window), "Login");
-    gtk_window_set_default_size(GTK_WINDOW(window), 300, 200);
     g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
 
-    // Create username entry
-    username_entry = gtk_entry_new();
-    gtk_entry_set_placeholder_text(GTK_ENTRY(username_entry), "Username");
-
-    // Create password entry
-    password_entry = gtk_entry_new();
-    gtk_entry_set_placeholder_text(GTK_ENTRY(password_entry), "Password");
-    gtk_entry_set_visibility(GTK_ENTRY(password_entry), FALSE);
-
-    // Create login button
-    login_button = gtk_button_new_with_label("Login");
-    g_signal_connect(login_button, "clicked", G_CALLBACK(on_login_button_clicked), this);
-
-    // Create layout
     GtkWidget *vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 5);
     gtk_container_add(GTK_CONTAINER(window), vbox);
+
+    username_entry = gtk_entry_new();
     gtk_box_pack_start(GTK_BOX(vbox), username_entry, FALSE, FALSE, 5);
+
+    password_entry = gtk_entry_new();
+    gtk_entry_set_visibility(GTK_ENTRY(password_entry), FALSE); // Hide password
     gtk_box_pack_start(GTK_BOX(vbox), password_entry, FALSE, FALSE, 5);
+
+    GtkWidget *login_button = gtk_button_new_with_label("Login");
     gtk_box_pack_start(GTK_BOX(vbox), login_button, FALSE, FALSE, 5);
+    g_signal_connect(login_button, "clicked", G_CALLBACK(on_login_button_clicked), this);
+}
+
+void LoginWindow::on_login_button_clicked(GtkButton *button, LoginWindow *login_window) {
+    const gchar *username = gtk_entry_get_text(GTK_ENTRY(login_window->username_entry));
+    const gchar *password = gtk_entry_get_text(GTK_ENTRY(login_window->password_entry));
+
+    // Add your login authentication logic here
 }
 
 GtkWidget* LoginWindow::get_window() {
     return window;
-}
-
-void LoginWindow::on_login_button_clicked(GtkButton *button, LoginWindow *login_window) {
-    // Retrieve username and password from entries
-    const gchar *username = gtk_entry_get_text(GTK_ENTRY(login_window->username_entry));
-    const gchar *password = gtk_entry_get_text(GTK_ENTRY(login_window->password_entry));
-
-    // Perform authentication (you'll replace this with your database interaction code)
-    if (g_strcmp0(username, "admin") == 0 && g_strcmp0(password, "password") == 0) {
-        g_print("Login successful!\n");
-    } else {
-        g_print("Login failed. Invalid username or password.\n");
-    }
 }
--- src/signup_window.cpp
+++ src/signup_window.cpp
@@ -1,42 +1,33 @@
+#include <gtk/gtk.h>
 #include "signup_window.h"
 
 SignupWindow::SignupWindow() {
-    // Create the window
     window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-    gtk_window_set_title(GTK_WINDOW(window), "Signup");
-    gtk_window_set_default_size(GTK_WINDOW(window), 300, 200);
+    gtk_window_set_title(GTK_WINDOW(window), "Sign Up");
     g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
 
-    // Create username entry
-    username_entry = gtk_entry_new();
-    gtk_entry_set_placeholder_text(GTK_ENTRY(username_entry), "Username");
-
-    // Create password entry
-    password_entry = gtk_entry_new();
-    gtk_entry_set_placeholder_text(GTK_ENTRY(password_entry), "Password");
-    gtk_entry_set_visibility(GTK_ENTRY(password_entry), FALSE);
-
-    // Create signup button
-    signup_button = gtk_button_new_with_label("Signup");
-    g_signal_connect(signup_button, "clicked", G_CALLBACK(on_signup_button_clicked), this);
-
-    // Create layout
     GtkWidget *vbox = gtk_box_new(GTK_ORIENTATION_VERTICAL, 5);
     gtk_container_add(GTK_CONTAINER(window), vbox);
+
+    username_entry = gtk_entry_new();
     gtk_box_pack_start(GTK_BOX(vbox), username_entry, FALSE, FALSE, 5);
+
+    password_entry = gtk_entry_new();
+    gtk_entry_set_visibility(GTK_ENTRY(password_entry), FALSE); // Hide password
     gtk_box_pack_start(GTK_BOX(vbox), password_entry, FALSE, FALSE, 5);
+
+    GtkWidget *signup_button = gtk_button_new_with_label("Sign Up");
     gtk_box_pack_start(GTK_BOX(vbox), signup_button, FALSE, FALSE, 5);
+    g_signal_connect(signup_button, "clicked", G_CALLBACK(on_signup_button_clicked), this);
+}
+
+void SignupWindow::on_signup_button_clicked(GtkButton *button, SignupWindow *signup_window) {
+    const gchar *username = gtk_entry_get_text(GTK_ENTRY(signup_window->username_entry));
+    const gchar *password = gtk_entry_get_text(GTK_ENTRY(signup_window->password_entry));
+
+    // Add your sign up logic here
 }
 
 GtkWidget* SignupWindow::get_window() {
     return window;
-}
-
-void SignupWindow::on_signup_button_clicked(GtkButton *button, SignupWindow *signup_window) {
-    // Retrieve username and password from entries
-    const gchar *username = gtk_entry_get_text(GTK_ENTRY(signup_window->username_entry));
-    const gchar *password = gtk_entry_get_text(GTK_ENTRY(signup_window->password_entry));
-
-    // Perform signup (you'll replace this with your database interaction code)
-    g_print("Signup functionality will be implemented here.\n");
 }
